d7c8eadc943693a95d75eccde7d2c9272620f521
joffitzer <joffitzer@gmail.com>
2020-01-31 12:01:35 -0500

    added ability to submit a new application and render to my applications page

--- src/components/JobShow.js
+++ src/components/JobShow.js
@@ -1,19 +1,40 @@
 import React from 'react'
 import { connect as cnx } from 'react-redux';
+import UserAppForm from '../components/UserAppForm'
 
-const JobShow = (props) => {
+class JobShow extends React.Component {
 
-    return(
-        <div>
-            <h5>Job Show Page</h5>
-            <h5>Employer: {props.jobToShow.attributes.employer.name}</h5> 
-            <h5>Title: {props.jobToShow.attributes.title}</h5>
-            <h5>Location: {props.jobToShow.attributes.location}</h5>
-            <h5>Category: {props.jobToShow.attributes.category}</h5>
-            <h5>Summary: {props.jobToShow.attributes.summary}</h5>
-            <button>APPLY TO THIS JOB</button>
-        </div>
-    )
+    state = {
+        applyClicked: false,
+    }
+
+    clickApply = () => {
+        this.setState({
+            applyClicked: !this.state.applyClicked
+        })
+    }
+
+    render() {
+
+        console.log('state of apply clicked', this.state.applyClicked)
+
+        return(
+            <div>
+                <h5>Job Show Page</h5>
+                <h5>Employer: {this.props.jobToShow.attributes.employer.name}</h5> 
+                <h5>Title: {this.props.jobToShow.attributes.title}</h5>
+                <h5>Location: {this.props.jobToShow.attributes.location}</h5>
+                <h5>Category: {this.props.jobToShow.attributes.category}</h5>
+                <h5>Summary: {this.props.jobToShow.attributes.summary}</h5>
+                <button onClick={this.clickApply}>APPLY TO THIS JOB</button>
+                {this.state.applyClicked ? 
+                <UserAppForm /> : 
+                null}
+            </div>
+        )
+
+    }
+
 }
 
 const mapStateToProps = (state) => {
---
+++ src/components/UserAppForm.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { connect as cnx } from 'react-redux';
+
+class UserAppForm extends React.Component {
+
+    state = {
+        miniCl: ""
+    }
+
+    handleSubmitApplication = (e) => {
+        console.log('submitting application')
+        e.preventDefault();
+        fetch ('http://localhost:3000/api/v1/user_apps', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json', 
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                mini_cl: this.state.miniCl,
+                job_id: this.props.jobToShow.id,
+                user_id: this.props.loggedInUser.id
+            })
+        }).then(this.setState({
+            miniCl: ""
+        }))
+    }
+
+    handleMiniClChange = (e) => {
+        this.setState({
+            miniCl: e.target.value
+        })
+    }
+
+    render() {
+
+        console.log('job in user app form: ', this.props.jobToShow)
+        console.log('user in user app form: ', this.props.loggedInUser)
+
+        return(
+            <form onSubmit={this.handleSubmitApplication}>
+                <label>
+                Mini Cover Letter, max xyz characters:
+                <input type="text" value={this.state.miniCl} onChange={this.handleMiniClChange} />
+                </label>
+    
+                <input type="submit" value="Submit" />
+            </form>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    let { jobToShow, loggedInUser } = state;
+    return {
+        jobToShow, loggedInUser
+    }
+  }
+
+export default cnx(mapStateToProps, null)(UserAppForm);
