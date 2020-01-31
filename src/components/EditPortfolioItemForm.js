b116fa98304336bd341df2fd0d5155085cc2004a
joffitzer <joffitzer@gmail.com>
2020-01-31 14:46:16 -0500

    added patch and destroy for portfolio items

---
+++ src/components/EditPortfolioItemForm.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { connect as cnx } from 'react-redux';
+
+class EditPortfolioItemForm extends React.Component {
+
+    state = {
+        titleValue: this.props.existingItem.attributes.title,
+        blurbValue: this.props.existingItem.attributes.blurb,
+        urlValue: this.props.existingItem.attributes.git_url
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        fetch (`http://localhost:3000/api/v1/portfolio_items/${this.props.existingItem.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json', 
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                title: this.state.titleValue,
+                blurb: this.state.blurbValue,
+                git_url: this.state.urlValue,
+                user_id: this.props.loggedInUser.id
+            })
+        }).then(this.setState({
+            titleValue: "",
+            blurbValue: "",
+            urlValue: ""
+        }))
+    }
+
+    handleTitleChange = (e) => {
+        this.setState({
+            titleValue: e.target.value
+        })
+    }
+
+    handleBlurbChange = (e) => {
+        this.setState({
+            blurbValue: e.target.value
+        })
+    }
+
+    handleUrlChange = (e) => {
+        this.setState({
+            urlValue: e.target.value
+        })
+    }
+
+
+    render() {
+
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                Title:
+                <input type="text" value={this.state.titleValue} onChange={this.handleTitleChange} />
+                </label>
+                <label>
+                Blurb:
+                <input type="text" value={this.state.blurbValue} onChange={this.handleBlurbChange} />
+                </label>
+                <label>
+                Github Url:
+                <input type="text" value={this.state.urlValue} onChange={this.handleUrlChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    let { loggedInUser } = state;
+    return {
+        loggedInUser
+    }
+  }
+
+export default cnx(mapStateToProps, null)(EditPortfolioItemForm);
--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,16 +1,42 @@
 import React from 'react'
+import EditPortfolioItemForm from './EditPortfolioItemForm'
 
-const PortfolioCard = (props) => {
+class PortfolioCard extends React.Component {
 
-    return(
-        <div>
-            <h5>Portfolio Card</h5>
-            <h5>Title: {props.item.attributes.title}</h5> 
-            <h5>Blurb: {props.item.attributes.blurb}</h5>              
-            <h5>GitHub Url: {props.item.attributes.git_url}</h5>                           
-            <hr></hr>
-        </div>
-    )
+    state = {
+        editClicked: false
+    }
+
+    loadEditForm = () => {
+        this.setState({
+            editClicked: true
+        })
+    }
+
+    deleteItem = () => {
+        fetch(`http://localhost:3000/api/v1/portfolio_items/${this.props.item.id}`,  {
+            method: 'DELETE'
+        })
+    }
+
+    render() {
+    
+        console.log('portfolio card props', this.props)
+    
+    
+        return(
+            <div>
+                <h5>Portfolio Card</h5>
+                <h5>Title: {this.props.item.attributes.title}</h5> 
+                <h5>Blurb: {this.props.item.attributes.blurb}</h5>              
+                <h5>GitHub Url: {this.props.item.attributes.git_url}</h5> 
+                <button onClick={this.loadEditForm}>Edit this portfolio item</button>  
+                {this.state.editClicked ? <EditPortfolioItemForm existingItem={this.props.item}/> : null}   
+                <button onClick={this.deleteItem}>Delete this portfolio item</button>                                               
+                <hr></hr>
+            </div>
+        )
+    }
 }
 
 export default PortfolioCard
