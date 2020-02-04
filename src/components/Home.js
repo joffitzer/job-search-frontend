3e11f61457a61d61568ac4895800dc5dbc457f70
joffitzer <joffitzer@gmail.com>
2020-02-04 17:35:48 -0500

    rendering search bar on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,54 @@
 import React from 'react'
 
-const Home = () => {
-    return(
-        <div>
-            <h1>Home Page </h1>
-        </div>
-    )
+class Home extends React.Component {
+
+    state = {
+        jobType: "",
+        location: ""
+    }
+
+    handleJobTypeChange = (e) => {
+        this.setState({
+            jobType: e.target.value
+        })
+    }
+
+    handleLocationChange = (e) => {
+        this.setState({
+            location: e.target.value
+        })
+    }
+
+    render(){
+
+        console.log(this.state)
+
+        return(
+            <div>
+                <h1>Home Page </h1>
+                    <span> I'm looking for 
+    
+                                <select value={this.state.jobType} onChange={this.handleJobTypeChange}>
+                                <option value="All Jobs">All Jobs</option>
+                                <option value="Software Engineering">Software Engineering</option>
+                                <option value="Data Science">Data Science</option>
+                                <option value="UX/UI Design">UX/UI Design</option>
+                                </select>
+                         
+                        {"  "}in{"  "} 
+                        
+                                <select value={this.state.location} onChange={this.handleLocationChange}>
+                                <option value="Any Location">Any Location</option>
+                                <option value="New York City">New York City</option>
+                                <option value="San Francisco">San Francisco</option>
+                                <option value="Chicago">Chicago</option>
+                                </select>
+                       
+                                <button>Search Jobs</button>
+                    </span>
+            </div>
+        )
+    }
 }
 
 export default Home;
