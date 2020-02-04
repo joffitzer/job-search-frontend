4075e970e706a97924504a045a9d08c48df46f65
joffitzer <joffitzer@gmail.com>
2020-02-04 15:18:13 -0500

    parsed ISO date format into day, month and year

--- src/components/UserAppCard.js
+++ src/components/UserAppCard.js
@@ -20,10 +20,25 @@ class UserAppCard extends React.Component {
             jobObj = this.props.allJobs.data.find(jobObj => parseInt(jobObj.id) === this.props.userApp.attributes.job.job.id)
         }
 
+        let date = new Date(this.props.userApp.attributes.created_at);
+        let year = date.getFullYear();
+        let month = date.getMonth()+1;
+        let dt = date.getDate();
+
+        if (dt < 10) {
+            dt = '0' + dt;
+        }
+        if (month < 10) {
+            month = '0' + month;
+        }
+
+        let applyDate = month + '/' + dt + '/' + year
+        
+
         return(
             <div>
                 <h5>User App Card</h5>
-                    <h5>You submitted this application on: {this.props.userApp.attributes.created_at}</h5>
+                    <h5>You submitted this application on: {applyDate}</h5>
                     <h5>Job Title: {this.props.userApp.attributes.job.job.title}</h5>
                     <h5>Employer: {this.props.userApp.attributes.job.employer.name}</h5>
                     <h5>Your Mini Cover Letter: {this.props.userApp.attributes.mini_cl}</h5>
