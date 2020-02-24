55cf71d58e89e1fcaf7bfc8e12371f3f09d8a549
joffitzer <joffitzer@gmail.com>
2020-02-24 18:55:02 -0500

    added commas to salary estimates

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -5,24 +5,35 @@ import { showJob } from '../actionCreators'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 
-const JobCard = (props) => {
+
+class JobCard extends React.Component{
+
+  numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  render() {
+
+    let salRangeLow = this.numberWithCommas(this.props.job.attributes.sal_range_low)
+    let salRangeHigh = this.numberWithCommas(this.props.job.attributes.sal_range_high)
 
     return(
             <Container>
-                    <font size="4">{props.job.attributes.employer.name}</font><br />
-                    <img className="cardLogo" src={props.job.attributes.employer.logo} alt="logo"/>
-                    <font size="3"><b>{props.job.attributes.title}</b></font><br />
-                    <font size="2"><i>{props.job.attributes.location}</i></font><br />
-                    <font size="2">{props.job.attributes.category}</font><br />
-                    <font size="2">{props.job.attributes.summary}</font><br />
-                    <font size="2"><i>${props.job.attributes.sal_range_low} - ${props.job.attributes.sal_range_high}</i></font><br />
+                    <font size="4">{this.props.job.attributes.employer.name}</font><br />
+                    <img className="cardLogo" src={this.props.job.attributes.employer.logo} alt="logo"/>
+                    <font size="3"><b>{this.props.job.attributes.title}</b></font><br />
+                    <font size="2"><i>{this.props.job.attributes.location}</i></font><br />
+                    <font size="2">{this.props.job.attributes.category}</font><br />
+                    <font size="2">{this.props.job.attributes.summary}</font><br />
+                    <font size="2"><i>${salRangeLow} - ${salRangeHigh}</i></font><br />
                 
-                    <Link to={`/jobs/${props.job.id}`}>
-                        <Button variant="primary" onClick={() => props.showJob(props.job)}>View Job Details and Apply</Button>
+                    <Link to={`/jobs/${this.props.job.id}`}>
+                        <Button variant="primary" onClick={() => this.props.showJob(this.props.job)}>View Job Details and Apply</Button>
                     </Link>
                     <hr></hr>
             </Container>
     )
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
